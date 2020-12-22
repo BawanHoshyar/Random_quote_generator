d2b0fbf5fa6dd24666306c3901e0933c5714401a
CaptCannoli <captcannoli@pop-os.localdomain>
2020-12-22 13:35:18 +0300

    this is the first React project

---
+++ src/components/Quote.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react'
+import axios from "axios";
+import './Quote.css';
+
+export default class Quote extends Component {
+
+    constructor(props) {
+        super(props)
+        this.generateNewQuote = this.generateNewQuote.bind(this);
+    }
+    state = {
+        
+    }
+
+    generateNewQuote() {
+        axios.get('http://api.quotable.io/random').then(
+            res => {
+                this.setState(res.data);
+                console.log(this.state);
+            }
+        ).catch(err => console.log(err))
+    }
+
+    componentDidMount() {
+        axios.get('http://api.quotable.io/random').then(
+            res => {
+                this.setState(res.data);
+                console.log(this.state);
+            }
+        ).catch(err => console.log(err))
+    }
+    render() {
+        return (
+            <div id="quote-box">
+                <p id="text">
+                    {this.state.content}
+                </p>
+                <h3 id="author">
+                    - {this.state.author}
+                </h3>
+                <div className="buttons">
+                <button id="new-quote" onClick={this.generateNewQuote}>New Quote</button>
+                <a href={`https://twitter.com/intent/tweet?text= ${this.state.content} - ${this.state.author}`} id="tweet-quote">
+                    Tweet
+                </a>
+                </div>
+            </div>
+        )
+    }
+}
+
